9976c911c6b85b4a513749495d2b9dfdf056e0c4
jeandiangelis <jean.angelis@gmail.com>
2016-12-11 22:44:20 -0300

    Added words data provider to the tests

--- test/HumanReadableGeneratorTest.php
+++ test/HumanReadableGeneratorTest.php
@@ -10,18 +10,18 @@ use PHPUnit\Framework\TestCase;
  */
 class HumanReadableGeneratorTest extends TestCase
 {
-    /**
-     * @var array
-     */
-    private $words = [
-        ['word', 'test', 'newTest'],
-        ['dig', 'a', 'ham'],
-        ['word'],
-        ['home', 'twister', 'phone', 'you', 'tv', 'box', 'eyeball'],
-        ['grass', 'bottle', 'dishes', 'bag', 'washing'],
-        ['pc', 'door']
-    ];
-
+    public static function getWords()
+    {
+        return [[[
+            ['word', 'test', 'newTest'],
+            ['dig', 'a', 'ham'],
+            ['word'],
+            ['home', 'twister', 'phone', 'you', 'tv', 'box', 'eyeball'],
+            ['grass', 'bottle', 'dishes', 'bag', 'washing'],
+            ['pc', 'door']
+        ]]];
+    }
+    
     /**
      * @expectedException \Exception\EmptyWordsException
      */
@@ -30,30 +30,20 @@ class HumanReadableGeneratorTest extends TestCase
         (new HumanReadableGenerator([], 1, 1));
     }
 
-    public function testPasswordMinimumSize()
+    public function testGenerateMatchesMinimumSize()
     {
         $password = (new HumanReadableGenerator(['dig', 'a', 'ham'], 1, 1))->generate();
 
         $this->assertTrue(strlen($password) > 5);
     }
 
-    public function testAssfsfa()
+    /**
+     * @dataProvider getWords
+     * @param $wordsArray
+     */
+    public function testGenerateHasUppercaseCharacter($wordsArray)
     {
-        $password = (new HumanReadableGenerator(['dig'], 8, 8))->generate();
-        $this->assertTrue((bool)preg_match('/[A-Z]/', $password));
-    }
-
-    public function testPasswordHasSpecialCharacter()
-    {
-        foreach ($this->words as $words) {
-            $password = (new HumanReadableGenerator($words, rand(1, 10), rand(1, 10)))->generate();
-            $this->assertTrue(!ctype_alnum($password));
-        }
-    }
-
-    public function testPasswordHasUppercaseChar()
-    {
-        foreach ($this->words as $words) {
+        foreach ($wordsArray as $words) {
             $strength =  rand(0, 10);
             $complexity =  rand(0, 10);
             $password = (new HumanReadableGenerator($words, $strength, $complexity))->generate();
@@ -61,9 +51,13 @@ class HumanReadableGeneratorTest extends TestCase
         }
     }
 
-    public function testPasswordHasLowercaseChar()
+    /**
+     * @dataProvider getWords
+     * @param array $wordsArray
+     */
+    public function testGenerateHasLowercaseChar($wordsArray)
     {
-        foreach ($this->words as $words) {
+        foreach ($wordsArray as $words) {
             $strength =  rand(0, 10);
             $complexity =  rand(0, 10);
             $password = (new HumanReadableGenerator($words, $strength, $complexity))->generate();
@@ -71,9 +65,13 @@ class HumanReadableGeneratorTest extends TestCase
         }
     }
 
-    public function testPasswordHasSpecialChar()
+    /**
+     * @dataProvider getWords
+     * @param array $wordsArray
+     */
+    public function testGenerateHasSpecialChar($wordsArray)
     {
-        foreach ($this->words as $words) {
+        foreach ($wordsArray as $words) {
             $strength =  rand(0, 10);
             $complexity =  rand(0, 10);
             $password = (new HumanReadableGenerator($words, $strength, $complexity))->generate();
